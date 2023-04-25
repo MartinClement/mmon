8ad6909427350b199d8af5714628e5817fbdcebf
MartinClement <martinclementjules@gmail.com>
2023-04-25 22:24:38 +0200

    main: start dedicaded components for platforms

---
+++ src/components/platforms/PlatformView.vue
@@ -0,0 +1,21 @@
+<template>
+  <circle :cx="x" :cy="y" :r="r" :fill="fill" class="platform" @click="handleExplore(c)"/>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const PLATFORM_TYPES = [
+  "blue",
+  "yellow",
+  "purple",
+  "orange",
+  "maroon",
+];
+
+const props = defineProps({
+  x: { type: Number, required: true },
+  y: { type: Number, required: true },
+  type: { type: String, required: true, validator: v => PLATFORM_TYPES.includes(v)},
+});
+</script>
